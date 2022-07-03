8fae82e663bbb8290a30682a8e2e386e0506200e
Allen Yu <yu.junhong02@gmail.com>
2022-07-02 17:07:26 -0700

    Skins page working

--- src/components/SkinList.js
+++ src/components/SkinList.js
@@ -1,35 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Accordion from "react-bootstrap/Accordion";
+import { fetch } from "whatwg-fetch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as outlineStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar as filledStar } from "@fortawesome/free-solid-svg-icons";
 
-function SkinCard() {
-  return (
-    <div>
+function SkinCard({ weaponSkins }) {
+  let skinCards = [];
+  skinCards = weaponSkins.map((skin, index) => {
+    let icon = skin.chromas[0].fullRender;
+    let name = skin.displayName;
 
-    </div>
-  );
+    return (
+      <div className="skin-card" key={index}>
+        <div className="skin-img">
+          <img src={icon} alt={name}></img>
+        </div>
+        <div className="skin-info">
+          <p className="info-text">{name}</p>
+          <FontAwesomeIcon className="info-text" icon={outlineStar} />
+        </div>
+      </div>
+    );
+  });
+  return <div className="skin-container">{skinCards}</div>;
 }
 
 function SkinList() {
-  let eventId = 0;
-//   let skinCards = skins.map((currSkin) => {
-//     return (
-//       <Accordion.Item eventKey={eventId}>
-//         <Accordion.Header>
-//           Hello
-//         </Accordion.Header>
-//         <Accordion.Body>
-//           <SkinCard skin={currSkin} />
-//         </Accordion.Body>
-//       </Accordion.Item>
-//     );
-//   });
+  const [weapons, setWeapons] = useState([]);
 
-  return (
-    <Accordion defaultActiveKey="0" alwaysOpen>
-      <div></div>
-    </Accordion>
-  );
+  const fetchWeapons = async () => {
+    await fetch("https://valorant-api.com/v1/weapons")
+      .then((res) => res.json())
+      .then((data) => {
+        setWeapons(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchWeapons();
+  }, []);
+
+  console.log(weapons);
+  let skinCards = [];
+  if (weapons.length > 0) {
+    skinCards = weapons.map((weapon, index) => {
+      return (
+        <Accordion.Item eventKey={index.toString()} key={index}>
+          <Accordion.Header>{weapon.displayName}</Accordion.Header>
+          <Accordion.Body>
+            <SkinCard weaponSkins={weapon.skins} />
+          </Accordion.Body>
+        </Accordion.Item>
+      );
+    });
+  }
+
+  return <Accordion>{skinCards}</Accordion>;
 }
 
 export default SkinList;
--- src/components/SkinPage.js
+++ src/components/SkinPage.js
@@ -1,22 +1,13 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar as outlineStar } from "@fortawesome/free-regular-svg-icons";
-import { faStar as filledStar } from "@fortawesome/free-solid-svg-icons";
+import SkinList from "./SkinList";
 
 function SkinPage() {
   return (
     <div className="skin-page page-padding">
       <h1 className="valorant-heading center">VALORANT SKINS</h1>
       <h2 className="center">Star the skin if you wish to be notify</h2>
-      <div className="skin-card">
-        <div className="skin-img">
-          <img src="./img/prime.png" alt="Prime Vandal"></img>
-        </div>
-        <div className="skin-info">
-          <p className="info-text">Prime Vandal</p>
-          <p className="info-text">VP: 1775</p>
-          <FontAwesomeIcon className="info-text" icon={outlineStar} />
-        </div>
+      <div className="accordion-container">
+        <SkinList />
       </div>
     </div>
   );
